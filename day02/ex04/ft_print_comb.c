2f4745c9ab96b33527e5a4733ef12a5b448482d0
SpiritsVS <cherifimaxime63@gmail.com>
2020-02-25 02:10:21 +0100

    exo04day02

---
+++ day02/ex04/ft_print_comb.c
@@ -0,0 +1,38 @@
+void ft_show(char a, char b, char c)
+{
+	ft_putchar(a);
+	ft_putchar(b);
+	ft_putchar(c);
+	if (a != '7')
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
+}
+
+void ft_print_comb(void)
+{
+	char a;
+	char b;
+	char c;
+
+	a = '0';
+	while (a <= '7')
+	{
+		b = a + 1;
+		while (b <= '8')
+		{
+			c = b + 1;
+			while (c <= '9')
+			{
+				ft_show(a, b, c);
+				c++;
+			}
+
+			b++;
+		}
+
+		a++;
+	}
+}
+
