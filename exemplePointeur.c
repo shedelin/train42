2ff24b23b2c4edf8373376653483a7816ec2a556
shedelin <steven.hedeline@gmail.com>
2020-08-14 18:46:15 +0200

    cours pointeur

--- exemplePointeur.c
+++ exemplePointeur.c
@@ -5,15 +5,21 @@ void ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-
+void ft_prout(int *ptr) 	// je declare une fonction qui s'appel ft_prout, elle prend en parametre un pointeur sur int
+{				// et ne renvoie rien
+	ft_putchar(*ptr); 	// j'appel la fonction ft_putchar et je lui envoie ce que pointe ptr
+				// *ptr : je vais chercher la valeur de la variable que pointe nbr
+}
 
 int main()
 {
-	int nb;
-	int *ptr;
+	int nb; 	// je declare une variable qui s'appel nb qui est un nombre
+	int *ptr; 	// je declare une variable qui s'appel ptr et qui est un pointeur sur int(un nombre entier)
 
-	nb = 9;
-	ptr = &nb;
-	ft_print(ptr);
+	nb = 67; 	// je donne la valeur 67 a la variable nb // 67 c'est le caractere C(table Ascii)
+	ptr = &nb; 	// je fais pointer le pointeur ptr sur la variable nb(qui est un int) 
+			//(&nb est l'adresse dans la memoire de la variable nb)
+	ft_prout(ptr); 	// j'apelle la fontion ft_prout et je lui envoie le pointeur ptr (pointeur sur un int)
+
 	return (0);
 }
